7ff287480123d2a40c6dd99fc827f3dd93f0d772
= <kimstudswcode4485@gmail.com>
2020-06-14 10:49:47 +0900

    .

--- deno.ts
+++ deno.ts
@@ -3,6 +3,6 @@ import { serve } from 'https://deno.land/std@0.50.0/http/server.ts';
 const server = serve({ port: 8000 }); // 원하는 Port 번호로 지정
 
 for await (const req of server) {
-    console.log('서버 실행 완료');
-    req.respond({body: 'Hello Deno!'});
+  console.log('서버 실행 완료');
+  req.respond({body: 'Hello Deno!'});
 }
