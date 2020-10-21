117fc5d83fa9f15fecdf12a585c88aaf70aa072c
KHJcode <kimstudswcode4485@gmail.com>
2020-10-22 00:38:20 +0900

    update readme

--- deno.ts
+++ deno.ts
@@ -1,8 +1,8 @@
-import { serve } from 'https://deno.land/std@0.50.0/http/server.ts';
+import { serve } from "https://deno.land/std@0.68.0/http/server.ts";
 
 const server = serve({ port: 8000 }); // 원하는 Port 번호로 지정
 
 for await (const req of server) {
   console.log('서버 실행 완료');
-  req.respond({body: 'Hello Deno!'});
+  req.respond({ body: 'Hello Deno!' });
 }
